2387af016c8a11aa069533b09e66ee2a36cb724f
Jovvy Bersamin <jovvybersamin99@gmail.com>
2020-02-24 07:26:03 +0800

    Added Col component

--- resources/js/src/views/Employees/List.js
+++ resources/js/src/views/Employees/List.js
@@ -117,21 +117,23 @@ const List = ({ loading: appLoading, setPageName }) => {
                 <Col span={12}>
                     <h1 className="text-3xl">Employees List</h1>
                 </Col>
-                <Row type="flex" align="middle" justify="end">
-                    <Col span={16}>
-                        <Input.Search
-                            allowClear
-                            placeholder="Search by name, customer"
-                            onSearch={handleSearchInput}
-                            style={{ lineHeight: 4 }}
-                        />
-                    </Col>
-                    <Col span={6}>
-                        <div className="flex justify-end">
-                            <Button type="primary" icon="plus" onClick={handleAddNewEmployeeClick}>ADD EMPLOYEE</Button>
-                        </div>
-                    </Col>
-                </Row>
+                <Col span={12}>
+                    <Row type="flex" align="middle" justify="end">
+                        <Col span={16}>
+                            <Input.Search
+                                allowClear
+                                placeholder="Search by name, customer"
+                                onSearch={handleSearchInput}
+                                style={{ lineHeight: 4 }}
+                            />
+                        </Col>
+                        <Col span={6}>
+                            <div className="flex justify-end">
+                                <Button type="primary" icon="plus" onClick={handleAddNewEmployeeClick}>ADD EMPLOYEE</Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
 
             <Table
